33986503983c8a29db9b2646f8b51eaba9e059ac
Kawin Cheong <cheong.kawin.dev@gmail.com>
2024-10-18 16:20:52 +0800

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/kawin_portfolio', // Change this to your GitHub repository name
-  assetPrefix: '/kawin_portfolio/', // Change this to your GitHub repository name
-  output: 'export', // Specify static export
-};
+const nextConfig = {};
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: '/kawin_portfolio', // Change this to your GitHub repository name
+//   assetPrefix: '/kawin_portfolio/', // Change this to your GitHub repository name
+//   output: 'export', // Specify static export
+// };
+
+// export default nextConfig;
