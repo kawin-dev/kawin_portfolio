000d7a9982e7ba86aa551a3529289751e2da1978
kawin-dev <cheong.kawin.dev@gmail.com>
2024-10-18 14:52:00 +0800

    Home: adjusted to center

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -18,20 +18,17 @@ const HomeComponent: React.FC = () => {
     >
       <Container maxWidth={false}
       sx={{
-        display: 'flex'
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
       >
-        {/* <Typography variant="h2"
-          sx={{
-            textAlign: 'center',
-          }}
-        >Kawin Cheong</Typography> */}
+
         <Image 
           src="/images/Kawin_Cheong.png" 
           alt="Logo" 
-          width={300} // Set the width
-          height={300} // Set the height
-          // style={{ marginRight: '16px' }} 
+          width={300}
+          height={300}
         />
         <Container
           sx={{
@@ -63,7 +60,7 @@ const HomeComponent: React.FC = () => {
               paddingRight: 0
             }}
           >
-            With over 5 years of hands-on experience in software development and production, I have built a solid foundation in delivering robust, scalable SaaS products across various industries. My work has consistently focused on delivering innovative solutions while mastering technologies in both backend and frontend development, utilizing tools like Laravel, React, MySQL, and AWS. I have a deep understanding of the development lifecycle, from design to deployment, which has allowed me to successfully manage projects that meet high performance and security standards.
+            With over 5 years of hands-on experience in software development and production, I have built a solid foundation in delivering robust, scalable SaaS products across various industries. My work has consistently focused on delivering innovative solutions while mastering technologies in both backend and frontend development, utilizing tools like Laravel, React, MySQL, and GCP. I have a deep understanding of the development lifecycle, from design to deployment, which has allowed me to successfully manage projects that meet high performance and security standards.
             <br></br><br></br>
             Now, I'm driven to elevate my career by specializing in Cybersecurity. As the world becomes increasingly digital, securing systems, data, and networks has become critical. I’m committed to expanding my expertise in this area to tackle the complex challenges of today's security landscape, and further sharpen my technical skills. My goal is to combine my software development background with cutting-edge cybersecurity knowledge to provide holistic, secure, and innovative solutions.
           </Typography>
